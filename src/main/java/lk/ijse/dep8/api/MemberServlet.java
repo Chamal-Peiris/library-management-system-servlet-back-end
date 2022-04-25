8fbddae021f241c00deaf726f1252323d10fbc15
Chamal-Peiris <chamal.peiris3g@gmail.com>
2022-04-25 15:44:53 +0530

    Add member DTO and Servlet

---
+++ src/main/java/lk/ijse/dep8/api/MemberServlet.java
@@ -0,0 +1,21 @@
+package lk.ijse.dep8.api;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "MemberServlet", value = {"/members","/members/"})
+public class MemberServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //validate the request
+
+    }
+}
---
+++ src/main/java/lk/ijse/dep8/dto/MemberDTO.java
@@ -0,0 +1,36 @@
+package lk.ijse.dep8.dto;
+
+import java.io.Serializable;
+
+public class MemberDTO implements Serializable {
+    private String nic;
+    private String name;
+    private String contact;
+
+    public MemberDTO() {
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
